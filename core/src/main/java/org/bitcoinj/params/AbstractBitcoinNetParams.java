6815e710fc76f3a2147af4f019edb71dd8e1d1af
HashEngineering <hashengineeringsolutions@gmail.com>
2018-03-21 23:39:43 -0700

    DAA:  Fix EDA when using checkpoints.

--- core/src/main/java/org/bitcoinj/params/AbstractBitcoinNetParams.java
+++ core/src/main/java/org/bitcoinj/params/AbstractBitcoinNetParams.java
@@ -95,6 +95,13 @@ public abstract class AbstractBitcoinNetParams extends NetworkParameters {
             }
             long mpt6blocks = 0;
             try {
+                //Check to see if there are enough blocks before cursor to correctly calculate the median time
+                StoredBlock beforeCursor = cursor;
+                for (int i = 0; i < 10; i++) {
+                    beforeCursor = blockStore.get(beforeCursor.getHeader().getPrevBlockHash());
+                    if(beforeCursor == null)
+                        return; //Not enough blocks to check difficulty.
+                }
                 mpt6blocks = blockChain.getMedianTimestampOfRecentBlocks(storedPrev, blockStore) - blockChain.getMedianTimestampOfRecentBlocks(cursor, blockStore);
             } catch (NullPointerException x)
             {
