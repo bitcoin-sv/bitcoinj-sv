c3c5d77c628818f899c4569f917e339bbf2db9d1
hashengineering <hashengineeringsolutions@gmail.com>
2018-03-23 22:17:48 -0700

    TestNet3Params:  remove inactive DNS Seeder

--- core/src/main/java/org/bitcoinj/params/TestNet3Params.java
+++ core/src/main/java/org/bitcoinj/params/TestNet3Params.java
@@ -57,7 +57,6 @@ public class TestNet3Params extends AbstractBitcoinNetParams {
         dnsSeeds = new String[] {
                "testnet-seed.bitcoinabc.org",
                 "testnet-seed-abc.bitcoinforks.org",
-                "testnet-seed.bitcoinunlimited.info",
                 "testnet-seed.bitprim.org",
                 "testnet-seed.deadalnix.me",
                 "testnet-seeder.criptolayer.net"
