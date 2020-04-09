8d5e4b3872b942d9e2a5e213259e300c6e8dc724
Steve Shadders <shadders.del@gmail.com>
2020-04-09 19:34:29 +0100

    add STN params

---
+++ core/src/main/java/org/bitcoinj/params/STNParams.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2013 Google Inc.
+ * Copyright 2014 Andreas Schildbach
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.params;
+
+import org.bitcoinj.core.Block;
+import org.bitcoinj.core.Utils;
+
+import java.util.Date;
+
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * Parameters for the testnet, a separate public instance of Bitcoin that has relaxed rules suitable for development
+ * and testing of applications and new Bitcoin versions.
+ */
+public class STNParams extends TestNet3Params {
+
+    public static final String ID_GBTN = "org.bitcoin.stn";
+
+    public STNParams() {
+        super();
+        id = ID_GBTN;
+        // Genesis hash is 000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943
+        //packetMagic = 0xf4e5f3f4L;
+        packetMagic = 0xfbcec4f9L;
+//        interval = INTERVAL;
+//        targetTimespan = TARGET_TIMESPAN;
+//        maxTarget = Utils.decodeCompactBits(0x1d00ffffL);
+        port = 9333;
+//        addressHeader = 111;
+//        p2shHeader = 196;
+//        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+//        dumpedPrivateKeyHeader = 239;
+//        genesisBlock.setTime(1296688602L);
+//        genesisBlock.setDifficultyTarget(0x1d00ffffL);
+//        genesisBlock.setNonce(414098458);
+//        spendableCoinbaseDepth = 100;
+//        subsidyDecreaseBlockCount = 210000;
+//        String genesisHash = genesisBlock.getHashAsString();
+//        checkState(genesisHash.equals("000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
+//        alertSigningKey = Utils.HEX.decode("04302390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a");
+
+        dnsSeeds = new String[] {
+                "stn-seed.bitcoinsv.io"
+        };
+//        addrSeeds = null;
+//        bip32HeaderPub = 0x043587CF;
+//        bip32HeaderPriv = 0x04358394;
+//
+//        majorityEnforceBlockUpgrade = TestNet2Params.TESTNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
+//        majorityRejectBlockOutdated = TestNet2Params.TESTNET_MAJORITY_REJECT_BLOCK_OUTDATED;
+//        majorityWindow = TestNet2Params.TESTNET_MAJORITY_WINDOW;
+
+        // Aug, 1 hard fork
+        uahfHeight = 16; //this value is 1 higher than in chainparams.cpp
+        // Nov, 13 hard fork
+
+    }
+
+    private static STNParams instance;
+    public static synchronized STNParams get() {
+        if (instance == null) {
+            instance = new STNParams();
+        }
+        return instance;
+    }
+
+    @Override
+    public String getPaymentProtocolId() {
+        return "stn";
+    }
+
+    // February 16th 2012
+    private static final Date testnetDiffDate = new Date(1329264000000L);
+
+    public static boolean isValidTestnetDateBlock(Block block){
+        return block.getTime().after(testnetDiffDate);
+    }
+
+}
+
