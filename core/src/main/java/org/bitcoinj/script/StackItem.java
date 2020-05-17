d3ee73fde7424b07c8d0d6f38675603fc2404a10
Steve Shadders <shadders.del@gmail.com>
2020-05-17 21:44:33 +0100

    render stack items according to type in toString() placeholder for user defined context object

--- core/src/main/java/org/bitcoinj/script/StackItem.java
+++ core/src/main/java/org/bitcoinj/script/StackItem.java
@@ -2,13 +2,17 @@ package org.bitcoinj.script;
 
 import org.bitcoinj.core.Utils;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Stack;
 
-public class StackItem {
+public class StackItem<C> {
 
     public final byte[] bytes;
     public final int length;
+    public final Type type;
+    public final C context;
     /**
      * true if the creation of this item is dependent on a value provided
      * to the script (was on the stack before the script started)
@@ -21,24 +25,26 @@ public class StackItem {
      * @return
      */
     public static StackItem wrap(byte[] bytes) {
-        return new StackItem(bytes, false);
+        return new StackItem(bytes, Type.BYTES, false);
     }
 
     public static StackItem wrapDerived(byte[] bytes, boolean derived) {
-        return new StackItem(bytes, true);
+        return new StackItem(bytes, Type.BYTES, true);
     }
 
     public static StackItem from(StackItem from, StackItem ... derivedFrom) {
-        return new StackItem(from.bytes, from.derived, derivedFrom);
+        return new StackItem(from.bytes, from.type, from.derived, derivedFrom);
     }
 
     public static StackItem from(byte[] bytes, StackItem ... derivedFrom) {
-        return new StackItem(bytes, false, derivedFrom);
+        return new StackItem(bytes, Type.BYTES, false, derivedFrom);
     }
 
-    private StackItem(byte[] bytes, boolean derived, StackItem ... derivedFrom) {
+    private StackItem(byte[] bytes, Type type, boolean derived, StackItem ... derivedFrom) {
         this.bytes = bytes;
+        this.type = type;
         this.length = bytes.length;
+        this.context = null;
         if (!derived) {
             for (StackItem item : derivedFrom) {
                 if (item.derived) {
@@ -49,10 +55,24 @@ public class StackItem {
         }
         this.derived = derived;
     }
+    /**
+     * @return the value as an integer if possible.
+     */
+    public BigInteger getInteger() {
+        return Utils.decodeMPI(bytes, false);
+    }
 
-//    public byte[] bytes() {
-//        return bytes;
-//    }
+    /**
+     * return backing bytes as a UTF-8 string
+     * @return
+     */
+    public String getAsString() {
+        try {
+            return new String(bytes, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public int length() {
         return bytes.length;
@@ -72,7 +92,19 @@ public class StackItem {
     }
 
     public String toString() {
-        return "0x" + Utils.HEX.encode(bytes);
+        String rendered = null;
+        if (type == StackItem.Type.STRING) {
+            rendered = getAsString();
+        } else if (type == StackItem.Type.INT) {
+            rendered = getInteger().toString();
+        } else {
+            rendered = "0x" + Utils.HEX.encode(bytes);
+        }
+        return type +": " + rendered;
+    }
+
+    public enum Type {
+        BYTES, INT, STRING;
     }
 }
 
