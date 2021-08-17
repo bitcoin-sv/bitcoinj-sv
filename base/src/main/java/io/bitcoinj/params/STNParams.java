53a136b6e13cfc02d0cc80ac2327c54939e0c8c0
q.zhou <q.zhou@nchain.com>
2021-08-17 20:16:22 +0800

    Change license of STNParams which should be new

--- base/src/main/java/io/bitcoinj/params/STNParams.java
+++ base/src/main/java/io/bitcoinj/params/STNParams.java
@@ -1,23 +1,7 @@
 /*
- * Modifications described in the NOTICE.txt file are licensed under the Open BSV Licence.
- * Modifications Copyright 2020 Bitcoin Association
- *
- * Copyright 2013 Google Inc.
- * Copyright 2014 Andreas Schildbach
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * © 2020 Bitcoin Association
+ * Open BSV Licence, see the accompanying file LICENSE
  */
-
 package io.bitcoinj.params;
 
 
