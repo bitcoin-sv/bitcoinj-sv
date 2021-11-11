e6e96797881948bed3816f537a8c7abee071bf55
Ivan J Lozano Fernandez <i.fernandez@nchain.com>
2021-11-11 13:41:07 +0100

    adding readUint64 operation

--- base/src/main/java/io/bitcoinj/core/Utils.java
+++ base/src/main/java/io/bitcoinj/core/Utils.java
@@ -299,6 +299,23 @@ public class Utils {
         return readUint32(readBytesStrict(in, 4), 0);
     }
 
+    /** Parse 8 bytes from the byte array (starting at the offset) as unsigned 64-bit integer in little endian format. */
+    public static long readUint64(byte[] bytes, int offset) {
+        return (bytes[offset] & 0xffl) |
+                ((bytes[offset + 1] & 0xffl) << 8) |
+                ((bytes[offset + 2] & 0xffl) << 16) |
+                ((bytes[offset + 3] & 0xffl) << 24) |
+                ((bytes[offset + 4] & 0xffl) << 32) |
+                ((bytes[offset + 5] & 0xffl) << 40) |
+                ((bytes[offset + 6] & 0xffl) << 48) |
+                ((bytes[offset + 7] & 0xffl) << 56);
+    }
+
+    /** Parse 8 bytes from the stream as unsigned 64-bit integer in little endian format. */
+    public static long readUint64(InputStream in) throws IOException {
+        return readUint64(readBytesStrict(in, 8), 0);
+    }
+
     /** Parse 6 bytes from the byte array (starting at the offset) as signed 64-bit integer in little endian format. */
     public static long readInt48(byte[] bytes, int offset) {
         return (bytes[offset] & 0xffl) |
